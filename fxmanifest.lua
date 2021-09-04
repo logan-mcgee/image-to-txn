ba1b482306a57400e034fdf6352f46e215c24b3a
logan <loganelliott@protonmail.com>
2021-09-04 01:43:17 +0100

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+files {
+  'img.html',
+  '*.png'
+}
+
+ui_page 'img.html'
+
+client_script 'img.js'
---
+++ img.js
@@ -0,0 +1,35 @@
+const waiting = {};
+
+function AddImage(url, txd, txn, cb) {
+  // generate a random id to store in an object
+  var id = Math.random().toString(36).substring(7);
+  waiting[id] = {
+    url: url,
+    txd,
+    txn,
+    cb
+  };
+
+  SendNuiMessage(JSON.stringify({
+    type: 'getimg',
+    id,
+    url
+  }))
+}
+
+RegisterNuiCallbackType('recvImage') // register the type
+on('__cfx_nui:recvImage', (data, cb) => {
+  cb({ got: 'it' })
+
+  const { id, imgData, width, height } = data;
+  const { txd, txn, cb: callback } = waiting[id];
+  if (!txd || !txn) return delete waiting[id];
+
+  const tex = CreateRuntimeTexture(txd, txn, width, height)
+  const uintData = new Uint8Array(imgData);
+  SetRuntimeTextureArgbData(tex, uintData, uintData.length);
+
+  //? if using a callback we call it in order to add a way of knowing when the txn is "ready"
+  if (callback) callback()
+  delete waiting[id];
+});
